cbe1efea859fc0c6e25bebc5d267547b939450e1
David Monfrooij <d.monfrooij@gmail.com>
2025-08-22 17:04:22 +0200

    env file

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
